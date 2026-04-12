5094abf7163300e4497852e26ecc4ca9fe846f6f
Ali Raza Mujahid <razadeveloper816@gmail.com>
2026-04-12 08:15:35 +0800

    add detail page

--- category.php
+++ category.php
@@ -3,125 +3,120 @@
     <div class="row gx-5">
         <div class="col-md-8 p-0">
             <h2 class='fs-5 ms-3 ms-md-0 my-5 text-secondary'>Category</h2>
-            <div class="card border-0 mb-3">
-                <div class="row g-0">
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-                            <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <p class="card-text">
-                                <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
-                                <small class="text-body-secondary">Jul 14</small>
-                            </p>
+            <a href="single.php" class='text-decoration-none'>
+                <div class="card border-0 mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
+                                <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <p class="card-text">
+                                    <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
+                                    <small class="text-body-secondary">Jul 14</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
-                    </div>
                 </div>
-            </div>
-            <div class="card border-0 mb-3">
-                <div class="row g-0">
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-                            <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <p class="card-text">
-                                <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
-                                <small class="text-body-secondary">Jul 14</small>
-                            </p>
+            </a>
+            <a href="single.php" class='text-decoration-none'>
+                <div class="card border-0 mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
+                                <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <p class="card-text">
+                                    <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
+                                    <small class="text-body-secondary">Jul 14</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
-                    </div>
                 </div>
-            </div>
-            <div class="card border-0 mb-3">
-                <div class="row g-0">
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-                            <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <p class="card-text">
-                                <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
-                                <small class="text-body-secondary">Jul 14</small>
-                            </p>
+            </a>
+            <a href="single.php" class='text-decoration-none'>
+                <div class="card border-0 mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
+                                <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <p class="card-text">
+                                    <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
+                                    <small class="text-body-secondary">Jul 14</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
-                    </div>
                 </div>
-            </div>
-            <div class="card border-0 mb-3">
-                <div class="row g-0">
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-                            <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <p class="card-text">
-                                <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
-                                <small class="text-body-secondary">Jul 14</small>
-                            </p>
+            </a>
+            <a href="single.php" class='text-decoration-none'>
+                <div class="card border-0 mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
+                                <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <p class="card-text">
+                                    <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
+                                    <small class="text-body-secondary">Jul 14</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
-                    </div>
                 </div>
-            </div>
-            <div class="card border-0 mb-3">
-                <div class="row g-0">
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-                            <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <p class="card-text">
-                                <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
-                                <small class="text-body-secondary">Jul 14</small>
-                            </p>
+            </a>
+            <a href="single.php" class='text-decoration-none'>
+                <div class="card border-0 mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
+                                <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <p class="card-text">
+                                    <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
+                                    <small class="text-body-secondary">Jul 14</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
-                    </div>
                 </div>
-            </div>
-            <div class="card border-0 mb-3">
-                <div class="row g-0">
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-                            <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <p class="card-text">
-                                <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
-                                <small class="text-body-secondary">Jul 14</small>
-                            </p>
+            </a>
+            <a href="single.php" class='text-decoration-none'>
+                <div class="card border-0 mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
+                                <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
+                                <p class="card-text">
+                                    <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
+                                    <small class="text-body-secondary">Jul 14</small>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
                         </div>
                     </div>
-                    <div class="col-md-4">
-                        <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
-                    </div>
                 </div>
-            </div>
-            <div class="card border-0 mb-3">
-                <div class="row g-0">
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-                            <p class="card-text fs-6 text-secondary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi temporibus praesentium neque, voluptatum quam quibusdam.</p>
-                            <p class="card-text">
-                                <small class="text-body-secondary fw-bold d-flex flex-column">Author: Ali Raza</small>
-                                <small class="text-body-secondary">Jul 14</small>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <img src="images/1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" class="img-fluid" alt="">
-                    </div>
-                </div>
-            </div>
+            </a>
         </div>
         <?php include "sidebar.php" ?>
     </div>
--- sidebar.php
+++ sidebar.php
@@ -1,44 +1,52 @@
 <div class="col-md-4">
     <h2 class='fs-4 text-dark my-5'>Popular Posts</h2>
-    <div class='d-flex mb-3 align-items-center'>
-        <h2 class='text-secondary fs-3 me-3'>01</h2>
-        <div class='p-2'>
-            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-            <div class='d-flex flex-column'>
-                <span style="font-size:15px;">Author: Ali Raza</span>
-                <span class='text-secondary'>Jul 14</span>
+    <a href="single.php" class='text-decoration-none text-dark'>
+        <div class='d-flex mb-3 align-items-center'>
+            <h2 class='text-secondary fs-3 me-3'>01</h2>
+            <div class='p-2'>
+                <h5 class="card-title fs-6 mb-2">News Needs to Meet Its Audiences Where They Are</h5>
+                <div class='d-flex flex-column'>
+                    <span class='fw-bold text-secondary'>Author: Ali Raza</span>
+                    <span class='text-secondary'>Jul 14</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class='d-flex mb-3 align-items-center'>
-        <h2 class='text-secondary fs-3 me-3'>02</h2>
-        <div class='p-2'>
-            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-            <div class='d-flex flex-column'>
-                <span style="font-size:15px;">Author: Ali Raza</span>
-                <span class='text-secondary'>Jul 14</span>
+    </a>
+    <a href="single.php" class='text-decoration-none text-dark'>
+        <div class='d-flex mb-3 align-items-center'>
+            <h2 class='text-secondary fs-3 me-3'>02</h2>
+            <div class='p-2'>
+                <h5 class="card-title fs-6 mb-2">News Needs to Meet Its Audiences Where They Are</h5>
+                <div class='d-flex flex-column'>
+                    <span class='fw-bold text-secondary'>Author: Ali Raza</span>
+                    <span class='text-secondary'>Jul 14</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class='d-flex mb-3 align-items-center'>
-        <h2 class='text-secondary fs-3 me-3'>03</h2>
-        <div class='p-2'>
-            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-            <div class='d-flex flex-column'>
-                <span style="font-size:15px;">Author: Ali Raza</span>
-                <span class='text-secondary'>Jul 14</span>
+    </a>
+    <a href="single.php" class='text-decoration-none text-dark'>
+        <div class='d-flex mb-3 align-items-center'>
+            <h2 class='text-secondary fs-3 me-3'>03</h2>
+            <div class='p-2'>
+                <h5 class="card-title fs-6 mb-2">News Needs to Meet Its Audiences Where They Are</h5>
+                <div class='d-flex flex-column'>
+                    <span class='fw-bold text-secondary'>Author: Ali Raza</span>
+                    <span class='text-secondary'>Jul 14</span>
+                </div>
             </div>
         </div>
-    </div>
-    <div class='d-flex mb-3 align-items-center'>
-        <h2 class='text-secondary fs-3 me-3'>04</h2>
-        <div class='p-2'>
-            <h5 class="card-title fs-6">News Needs to Meet Its Audiences Where They Are</h5>
-            <div class='d-flex flex-column'>
-                <span style="font-size:15px;">Author: Ali Raza</span>
-                <span class='text-secondary'>Jul 14</span>
+    </a>
+    <a href="single.php" class='text-decoration-none text-dark'>
+        <div class='d-flex mb-3 align-items-center'>
+            <h2 class='text-secondary fs-3 me-3'>04</h2>
+            <div class='p-2'>
+                <h5 class="card-title fs-6 mb-2">News Needs to Meet Its Audiences Where They Are</h5>
+                <div class='d-flex flex-column'>
+                    <span class='fw-bold text-secondary'>Author: Ali Raza</span>
+                    <span class='text-secondary'>Jul 14</span>
+                </div>
             </div>
         </div>
-    </div>
+    </a>
     <a href="" class='text-uppercase' style='font-size:15px;color:#8BC34A;'>See All Popular<i class='fa-solid fa-angle-right'></i></a>
 </div>
