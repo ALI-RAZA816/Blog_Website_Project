871d2213431eae124baf778a0ed09ca29a29ad5d
Ali Raza Mujahid <razadeveloper816@gmail.com>
2026-04-14 07:40:15 +0800

    add cards in dashboard indexpage

--- admin/dashboard/header.php
+++ admin/dashboard/header.php
@@ -1,1 +1,10 @@
-<h1>Header</h1>
+<header class='d-flex py-2 justify-content-between align-items-center px-3' id="header">
+    <h2 class='fw-bold fs-3 mb-0 text-secondary'>Dashboard</h2>
+    <div class='accounts d-flex justify-content-between align-items-center'>
+        <div class='d-flex justify-content-between align-items-center search-bar'>
+            <i class='fa-solid fa-magnifying-glass search-icon'></i>
+            <input type="text" class='form-control' placeholder='Search...'>
+        </div>
+        <div class="user ms-5"></div>
+    </div>
+</header>
--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -1,15 +1,40 @@
 <div class="cotainer-fluid">
-    <div class="row">
+    <div class="row g-0">
         <div class="col-2 vh-100 bg-white sidebar">
             <?php include "sidebar.php" ?>
         </div>
-        <div class="col-10">
+        <div class="col-10 border">
             <?php include "header.php" ?>
-            asd
-            asdf
-            spl_autoload_functionssadf
-            spl_autoload_functionssadfsd
-            fclosefsdf
+            <div class="row no-gutters g-0 p-0">
+                <div class="col-12 px-5 py-3">
+                    <div class="row gy-5 p-0">
+                        <div class ='col-3 px-3 '>
+                            <div class="d-flex flex-column cards bg-white px-3 py-3">
+                                <span class='fw-bold fs-2 mb-3'>Posts</span>
+                                <span class='fw-bold fs-4 text-secondary'>12,500</span>
+                            </div>
+                        </div>
+                        <div class ='col-3 px-3 '>
+                            <div class="d-flex flex-column cards bg-white px-3 py-3">
+                                <span class='fw-bold fs-2 mb-3'>Categories</span>
+                                <span class='fw-bold fs-4 text-secondary'>12,500</span>
+                            </div>
+                        </div>
+                        <div class ='col-3 px-3 '>
+                            <div class="d-flex flex-column cards bg-white px-3 py-3">
+                                <span class='fw-bold fs-2 mb-3'>Users</span>
+                                <span class='fw-bold fs-4 text-secondary'>12,500</span>
+                            </div>
+                        </div>
+                        <div class ='col-3 px-3 '>
+                            <div class="d-flex flex-column cards bg-white px-3 py-3">
+                                <span class='fw-bold fs-2 mb-3'>Admins</span>
+                                <span class='fw-bold fs-4 text-secondary'>12,500</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
