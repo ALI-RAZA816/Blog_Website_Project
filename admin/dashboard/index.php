b921be0ae6614462b0a77b92ee7d24fec145df83
Ali Raza Mujahid <razadeveloper816@gmail.com>
2026-04-13 20:49:54 +0800

    create dashboard folder in admin folder and style sidebar

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -1,1 +1,15 @@
-<?php include "sidebar.php" ?>
+<div class="cotainer-fluid">
+    <div class="row">
+        <div class="col-2 vh-100 bg-white sidebar">
+            <?php include "sidebar.php" ?>
+        </div>
+        <div class="col-10">
+            <?php include "header.php" ?>
+            asd
+            asdf
+            spl_autoload_functionssadf
+            spl_autoload_functionssadfsd
+            fclosefsdf
+        </div>
+    </div>
+</div>
--- admin/dashboard/sidebar.php
+++ admin/dashboard/sidebar.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin - Dashboard</title>
-    <!-- BOOTSTRAP CDN LINK -->
+    <title>Document</title>
+        <!-- BOOTSTRAP CDN LINK -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <!-- FONTAWESOME CDN LINK  -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -17,5 +17,16 @@
     <link rel="stylesheet" href="../../css/utils.css"/>
 </head>
 <body>
+    <div>
+        <h1 class='fs-5 mb-3 text-dark fw-bold'>The Editorial Architect</h1>
+        <ul class="nav flex-column sidebar-ul">
+            <li class="nav-item">
+                <a class="nav-link text-secondary rounded-1 mb-1" aria-current="page" href="#">Dashboard</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-secondary rounded-1 mb-1" href="#">Posts</a>
+            </li>
+        </ul>
+    </div>
 </body>
 </html>
