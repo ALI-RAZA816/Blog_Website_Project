199c4d5898001339c2387f2a7dcffccf5efbafa9
Ali Raza Mujahid <razadeveloper816@gmail.com>
2026-04-14 08:44:46 +0800

    add recent posts table

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -5,31 +5,69 @@
         </div>
         <div class="col-10 border">
             <?php include "header.php" ?>
-            <div class="row no-gutters g-0 p-0">
+            <div class="row g-0 p-0">
                 <div class="col-12 px-5 py-3">
                     <div class="row gy-5 p-0">
-                        <div class ='col-3 px-3 '>
+                        <div class ='col-3'>
                             <div class="d-flex flex-column cards bg-white px-3 py-3">
-                                <span class='fw-bold fs-2 mb-3'>Posts</span>
-                                <span class='fw-bold fs-4 text-secondary'>12,500</span>
+                                <span class='fw-bold fs-3 mb-3'>Posts</span>
+                                <span class='fw-bold fs-5 text-secondary'>12,500</span>
                             </div>
                         </div>
-                        <div class ='col-3 px-3 '>
+                        <div class ='col-3'>
                             <div class="d-flex flex-column cards bg-white px-3 py-3">
-                                <span class='fw-bold fs-2 mb-3'>Categories</span>
-                                <span class='fw-bold fs-4 text-secondary'>12,500</span>
+                                <span class='fw-bold fs-3 mb-3'>Categories</span>
+                                <span class='fw-bold fs-5 text-secondary'>12,500</span>
                             </div>
                         </div>
-                        <div class ='col-3 px-3 '>
+                        <div class ='col-3'>
                             <div class="d-flex flex-column cards bg-white px-3 py-3">
-                                <span class='fw-bold fs-2 mb-3'>Users</span>
-                                <span class='fw-bold fs-4 text-secondary'>12,500</span>
+                                <span class='fw-bold fs-3 mb-3'>Users</span>
+                                <span class='fw-bold fs-5 text-secondary'>12,500</span>
                             </div>
                         </div>
-                        <div class ='col-3 px-3 '>
+                        <div class ='col-3'>
                             <div class="d-flex flex-column cards bg-white px-3 py-3">
-                                <span class='fw-bold fs-2 mb-3'>Admins</span>
-                                <span class='fw-bold fs-4 text-secondary'>12,500</span>
+                                <span class='fw-bold fs-3 mb-3'>Admins</span>
+                                <span class='fw-bold fs-5 text-secondary'>12,500</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row p-0 g-0">
+                <div class="col-12 ">
+                    <div class="row p-0 g-0 d-flex justify-content-center">
+                        <div class="col-8 pe-4">
+                            <div class='bg-secondary-subtle p-2 rounded-3'>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th class="col-5 text-secondary" style='background-color:transparent;'>Atricle Detail</th>
+                                            <th class="col-2 text-secondary" style='background-color:transparent;'>Status</th>
+                                            <th class="col-3 text-secondary" style='background-color:transparent;'>Author</th>
+                                            <th class="col-2 text-secondary" style='background-color:transparent;'>Date</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <div class='row g-0 p-0'>
+                                                    <div class="col-3"><img src="../1ec99389-6d1a-4d68-bee3-38ab2100d489.jpg" alt=""></div>
+                                                    <div class="col-9"></div>
+                                                </div>
+                                            </td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                            <td>@mdo</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div>
+                                <h4>Recent Posts</h4>
                             </div>
                         </div>
                     </div>
